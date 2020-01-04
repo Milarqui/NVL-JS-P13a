16bc4ea64bf81f5029d0b0109a267ab5bf13a459
Milarqui <59261420+Milarqui@users.noreply.github.com>
2020-01-04 16:00:37 +0100

    Create ejercicio-13a.js

---
+++ ejercicio-13a.js
@@ -0,0 +1,21 @@
+var primos = [];
+
+function esPrimo(n) {
+  if (n > 3) {
+    for (var i = 2; i < n / 2; i++) {
+      if(n % i == 0) {
+        return False;
+      }
+    }
+  }
+  return True;
+}
+
+function primerosPrimos(n) {
+  var i = 2
+  while(primos.length < n) {
+    if(esPrimo(i)) {
+      primos.push(i);
+    }
+  }
+}
